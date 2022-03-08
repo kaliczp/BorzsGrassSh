1437075aafdd63764677d486bdd3bb15802cd202
Péter Kalicz <kaliczp@gmail.com>
2022-03-08 18:29:49 +0100

    Basin raster map as a variable
    For qick changes basin raster map name changed to a variable.

--- Watershed.sh
+++ Watershed.sh
@@ -1,14 +1,15 @@
 d.mon start=wx0
 d.rast Kozep_Digitalis_Domborzatmodell_1m_EOV
+basin=basin_1M
 # Watershed delineation
-r.watershed -s elevation=Kozep_Digitalis_Domborzatmodell_1m_EOV threshold=1000000 accumulation=accum_1M drainage=draindir_1M basin=basin_1M
+r.watershed -s elevation=Kozep_Digitalis_Domborzatmodell_1m_EOV threshold=1000000 accumulation=accum_1M drainage=draindir_1M basin=$basin
 d.erase
-d.rast basin_1M
+d.rast $basin
 # Relief calculation
 r.relief input=Kozep_Digitalis_Domborzatmodell_1m_EOV output=Kozep_shade
 # Basin boundaries
-r.to.vect -s basin_1M output=basin_1M type=area
+r.to.vect -s $basin output=$basin type=area
 # Plot
 d.erase
-d.his hue=basin_1M intensity=Kozep_shade brighten=40
-d.vect basin_1M type=boundary
+d.his hue=$basin intensity=Kozep_shade brighten=40
+d.vect $basin type=boundary
