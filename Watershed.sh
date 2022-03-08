bb83bf4d88cd599783f9b6cbae10a528641a2dfb
Péter Kalicz <kaliczp@gmail.com>
2022-03-08 10:07:32 +0100

    Better visualisation
    Relif data extracted and combined with basin rasters

--- Watershed.sh
+++ Watershed.sh
@@ -3,3 +3,6 @@ d.rast Kozep_Digitalis_Domborzatmodell_1m_EOV
 r.watershed -s elevation=Kozep_Digitalis_Domborzatmodell_1m_EOV threshold=1000000 accumulation=accum_1M drainage=draindir_1M basin=basin_1M
 d.erase
 d.rast basin_1M
+r.relief input=Kozep_Digitalis_Domborzatmodell_1m_EOV output=Kozep_shade
+d.erase
+d.his hue=basin_1M intensity=Kozep_shade brighten=40
