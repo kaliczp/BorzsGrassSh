07d951b1c06e4fe60f581c98fbc559e8252bb8c0
Péter Kalicz <kaliczp@gmail.com>
2022-03-08 10:15:43 +0100

    Catchment boundaries
    Cathcment boundaries extracted based on Mitasova.

--- Watershed.sh
+++ Watershed.sh
@@ -6,6 +6,9 @@ d.erase
 d.rast basin_1M
 # Relief calculation
 r.relief input=Kozep_Digitalis_Domborzatmodell_1m_EOV output=Kozep_shade
+# Basin boundaries
+r.to.vect -s basin_1M output=basin_1M type=area
 # Plot
 d.erase
 d.his hue=basin_1M intensity=Kozep_shade brighten=40
+d.vect basin_1M type=boundary
