47c485a5ce421b61edced0cd961c5adfaf3d580a
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2022-03-18 15:17:55 +0100

    Typo correction in srun
    Double dash necessary to the text switch.
    
    Send as a test job.

--- process.sh
+++ process.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -A ACCOUNT
-#SBATCH --qos=lowpri
+#SBATCH --qos=test
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=EMAIL
-#SBATCH --job-name=teszt
+#SBATCH --job-name=watershed
 #SBATCH -n 8
 #SBATCH --time=00:30
 #SBATCH -o slurm.out
@@ -16,6 +16,6 @@ export GRASS_BATCH_JOB=~/BorzsGrassSh/watershed.sh
 pwd; hostname; date
 OMP_NUM_THREADS=$SLURM_NTASKS
 module load gcc/9.2.0 python/3.8.2 libpng/1.6.13 fftw/3.3.3-gnu freetype mpfr/3.1.1 mpc/1.0.1
-srun grass -text ~/grassdata/DINPIkozepTeszt/PERMANENT
+srun grass --text ~/grassdata/DINPIkozepTeszt/PERMANENT
 unset GRASS_BATCH_JOB
 date
