6f3d40438b0669ae7f0a74e86ca4787788df83da
Péter Kalicz <kaliczp@gmail.com>
2022-03-22 11:11:07 +0100

    Del datata added to mapset

--- Watershed.sh
+++ Watershed.sh
@@ -1,6 +1,13 @@
 r.in.gdal input=~/watershed/Kozep_Digitalis_Domborzatmodell_1m_EOV.tif out=Kozep_Digitalis_Domborzatmodell_1m_EOV
 d.mon start=wx0
 d.rast Kozep_Digitalis_Domborzatmodell_1m_EOV
+
+r.in.gdal input=~/watershed/Del_Digitalis_Domborzatmodell_EOV_1m.tif out=Del_Digitalis_Domborzatmodell_EOV_1m
+g.region rast=Del_Digitalis_Domborzatmodell_EOV_1m
+d.erase
+d.rast Del_Digitalis_Domborzatmodell_EOV_1m
+
+
 basin=basin_1M
 # Watershed delineation
 r.watershed -s elevation=Kozep_Digitalis_Domborzatmodell_1m_EOV threshold=1000000 accumulation=accum_1M drainage=draindir_1M basin=$basin
